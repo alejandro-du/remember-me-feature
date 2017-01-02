be9f35eab241db8ce780254133f7eb1094e48e07
Alejandro Duarte <alejandro@vaadin.com>
2017-01-02 15:02:22 +0200

    Initial commit

---
+++ src/main/java/com/example/MyUI.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
+
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
